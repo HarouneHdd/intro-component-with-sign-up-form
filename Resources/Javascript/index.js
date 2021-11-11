7bd0ebf228aabe090586ace27f3eff6117e5be79
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2021-11-11 17:15:06 +0100

    Saving before adding warnings to input fields

---
+++ Resources/Javascript/index.js
@@ -0,0 +1,17 @@
+/*const form = document.getElementById('form');
+
+const fname = document.getElementById('firstName');
+const lname= document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    let messages = [];
+    if (fname === '' || fname == null) {
+        messages.push('First name is required');
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+    }
+})*/
