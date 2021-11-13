5c0ec8630d9b650ee65db28c02b223c8146ce0e4
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2021-11-13 10:22:29 +0100

    Add custom error display

--- Resources/Javascript/index.js
+++ Resources/Javascript/index.js
@@ -1,17 +1,74 @@
-/*const form = document.getElementById('form');
+const form = document.getElementById('form');
 
 const fname = document.getElementById('firstName');
 const lname= document.getElementById('lastName');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-form.addEventListener('submit', e => {
-    let messages = [];
-    if (fname === '' || fname == null) {
-        messages.push('First name is required');
-    }
 
-    if (messages.length > 0) {
-        e.preventDefault();
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+const checkInputs = () => {
+    const fnameVal = fname.value.trim();
+    const lnameVal = lname.value.trim();
+    const emailVal = email.value;
+    const passwordVal = password.value;
+
+    let errorInput;
+
+    if (fnameVal === '' || fnameVal == null) {
+        triggerError(fname);
     }
-})*/
+    else {
+        triggerCorrectInputBehavior(fname);
+    }
+    if (lnameVal === '' || lnameVal == null) {
+        triggerError(lname);
+    }
+    else {
+        triggerCorrectInputBehavior(lname);
+    }
+    if (!checkEmailValidity(emailVal)) {
+        triggerError(email);
+    }
+    else {
+        triggerCorrectInputBehavior(email);
+    }
+    if (passwordVal === '' || passwordVal == null) { 
+        triggerError(password);
+    }
+    else {
+        triggerCorrectInputBehavior(password);
+    }
+}
+
+const checkEmailValidity = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const triggerError = input => {
+    const customInput = input.parentElement;
+    const errorIcon = customInput.querySelector('img');
+    const errorMessage = customInput.querySelector('small');
+
+    errorIcon.style.visibility = "visible";
+    errorMessage.style.visibility = "visible";
+}
+
+const triggerCorrectInputBehavior = input => {
+    const customInput = input.parentElement;
+    const errorIcon = customInput.querySelector('img');
+    const errorMessage = customInput.querySelector('small');
+
+    if (errorIcon.style.visibility === "visible") {
+        errorIcon.style.visibility = "hidden";
+    }
+    if (errorMessage.style.visibility === "visible") {
+        errorMessage.style.visibility = "hidden";
+    }
+}
